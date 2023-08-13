d861b936cfd497b6a61a788de2f81f009ae59c0b
chromonic <52333793+chromonic@users.noreply.github.com>
2023-08-13 04:15:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,19 +11,34 @@ let dLegalChars = {
   'O': 'Player2'
 };
 
+const checkFullBoard = (lBoard) => {
+  let sum = 0;
+  for (let verticalIterator = 0; verticalIterator < 3; verticalIterator++) {
+    for (let horizontalIterator = 0; horizontalIterator < 3; horizontalIterator++) {
+      if (lBoard[verticalIterator][horizontalIterator] != '') {
+        sum++;
+      }
+    }
+  }
+  return sum;
+};
+
 const getInput = (cPlayerCharacter) => {
   let coords = prt( dLegalChars[cPlayerCharacter] + "'s turn -> ");
   coords = coords.split(" ");
   coords[0] = parseInt(coords[0]);
   coords[1] = parseInt(coords[1]);
 
-  if (lBoard[coords[1]][coords[0]] == '') {
-    lBoard[coords[1]][coords[0]] = cPlayerCharacter; 
+  if (checkFullBoard(lBoard) == 9) {
+    return;
   }
-  else {
+
+  if (! ( lBoard[coords[1]][coords[0]] == '' ) ) {
     console.log("Invalid coords.");
     getInput(cPlayerCharacter);
   }
+
+  lBoard[coords[1]][coords[0]] = cPlayerCharacter;
 };
 
 const checkWinner = (cPlayerCharacter, lBoard) => {
@@ -32,10 +47,8 @@ const checkWinner = (cPlayerCharacter, lBoard) => {
   // horizontal checking
   for (let verticalIterator = 0; verticalIterator < 3; verticalIterator++) {
     // check if every list item is the same
-    if (allSameCheck(lBoard[verticalIterator])) {
-      bResult = true;
-      break;
-    }
+    bResult = allSameCheck(lBoard[verticalIterator]);
+    if ( bResult ) { return bResult; }
   }
 
   // vertical checking
@@ -44,10 +57,8 @@ const checkWinner = (cPlayerCharacter, lBoard) => {
     for (let verticalIterator = 0; verticalIterator < 3; verticalIterator++) {
       tmpArr.push(lBoard[verticalIterator][horizontalIterator]);
     }
-    if (allSameCheck(tmpArr)) {
-      bResult = true;
-      break;
-    }
+    bResult = allSameCheck(tmpArr);
+    if ( bResult ) {return bResult;}
   }
 
   // diagonal checking
@@ -81,6 +92,8 @@ while (!bGameOver) {
   if (cPlayerCharacter == 'X') {cPlayerCharacter = 'O';}
   else {cPlayerCharacter = 'X';}
 
-  bGameOver = checkWinner(cPlayerCharacter, lBoard)
+  bGameOver = checkFullBoard(lBoard) == 9 ? true : false;
+
+  bGameOver = checkWinner(cPlayerCharacter, lBoard);
 }
 console.log("-- "+ cPlayerCharacter + " has won the game. --");
